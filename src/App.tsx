4cac9e83b70dba1a67d6027253e57a2e81e7af75
Daniel Deutsch <deudan1010@gmail.com>
2018-03-31 11:54:02 +0200

    Add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 
 import './App.css';
-
+import { AppStateType } from './types';
 import Chart from './Chart';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component {
+/* tslint:disable no-any */
+class App extends React.Component<any, AppStateType> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      currentUser: 'Daniel'
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -14,6 +21,7 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <h2>Welcome {this.state.currentUser}</h2>
         <Chart />
       </div>
     );
