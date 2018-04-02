35b7e6e72187c56291555be2d7a809464fcfe7c2
Daniel Deutsch <deudan1010@gmail.com>
2018-04-02 17:13:39 +0200

    Improve styling of chart

--- src/Chart/Chart.tsx
+++ src/Chart/Chart.tsx
@@ -10,24 +10,27 @@ import {
 } from 'recharts';
 
 import { ChartPropsType } from './types';
+import { ChartStyle } from './styles';
 
 const Chart: React.StatelessComponent<ChartPropsType> = (
   props: ChartPropsType
 ) => (
-  <LineChart
-    width={600}
-    height={300}
-    data={props.data}
-    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-  >
-    <XAxis dataKey="name" />
-    <YAxis />
-    <CartesianGrid strokeDasharray="3 3" />
-    <Tooltip />
-    <Legend />
-    <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-  </LineChart>
+  <ChartStyle>
+    <LineChart
+      width={600}
+      height={300}
+      data={props.data}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <XAxis dataKey="name" />
+      <YAxis />
+      <CartesianGrid strokeDasharray="3 3" />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+    </LineChart>
+  </ChartStyle>
 );
 
 export { Chart };
